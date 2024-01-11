938fb657fdef61a83b9a839fb51296ec7e562d02
Leo7373532 <113642342+Leo7373532@users.noreply.github.com>
2024-01-10 19:35:32 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleExpand() {
+    var expandableContainer = document.getElementById("expandableContainer");
+    expandableContainer.style.display = (expandableContainer.style.display === "none") ? "block" : "none";
+}
